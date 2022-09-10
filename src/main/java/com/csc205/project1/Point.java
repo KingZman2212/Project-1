7ab69b1ad612627bcf96fbe90428aacc5ecd7f35
KingZman2212 <zac2188946@maricopa.edu>
2022-09-10 14:24:26 -0700

    Initial commit

---
+++ src/main/java/com/csc205/project1/Point.java
@@ -0,0 +1,63 @@
+package com.csc205.project1;
+
+public class Point {
+   private double x;
+   private double y;
+
+   public Point () {
+       this.x = x;
+       this.y = y;
+   }
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p){
+        double distance = Math.sqrt(Math.pow((this.x - p.getX()), 2) + Math.pow((this.y - p.getY()), 2));
+        return distance;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setPoint(double new_x, double new_y){
+        this.x = new_x;
+        this.y = new_y;
+    }
+
+    public void shiftX(double dist){
+        this.x += dist;
+    }
+
+    public void shiftY(double dist){
+        this.y += dist;
+    }
+    public void rotate(double radian){
+        this.x = (x * (Math.cos(radian))) - (y * (Math.sin(radian)));
+        this.y = (x * (Math.sin(radian))) + (y * (Math.cos(radian)));
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x = " + x +
+                ", y = " + y +
+                '}';
+    }
+
+
+}
